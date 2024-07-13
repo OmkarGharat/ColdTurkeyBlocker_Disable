a4694e060a3c349bd375e3f0ff80596b65bc6f3e
Omkar Gharat <83211956+OmkarGharat@users.noreply.github.com>
2024-07-13 09:27:05 +0530

    Add files via upload

---
+++ cold turkey blocker disable.py
@@ -0,0 +1,73 @@
+import json
+import logging
+import os
+import sqlite3
+from pathlib import Path
+from typing import Any
+
+# Update this path to match the actual path on your Windows machine
+WIN_DB_PATH = Path(r"C:\ProgramData\Cold Turkey\data-app.db")
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s -> %(message)s")
+
+def blocker_summary(blocks: dict[str, Any]) -> None:
+    """Shows a summary of all blockers present in the app in terminal."""
+    logging.critical("Blockers Summary...")
+    print("--" * 40)
+    for blocker in blocks:
+        if blocker == "Frozen Turkey":  # Ignore Frozen Turkey block
+            continue
+        if blocks[blocker]["enabled"] == "true":
+            logging.info(f"Block Enabled:  {blocker}")
+        else:
+            logging.info(f"Block Disabled: {blocker}")
+    print("--" * 40)
+
+def disable_blocker(blocks: dict[str, Any]) -> dict[str, Any]:
+    print()
+    logging.critical("Disabling the blocker...")
+    print("--" * 40)
+    for blocker in blocks:
+        if blocker == "Frozen Turkey":  # Ignore Frozen Turkey block
+            continue
+        if blocks[blocker]["enabled"] == "true":
+            blocks[blocker]["enabled"] = "false"
+            logging.info(f"Block Disabled: {blocker}")
+    print("--" * 40)
+    return blocks
+
+def main():
+    db_path = WIN_DB_PATH
+
+    if not db_path.exists():
+        logging.info(f"{db_path = !s}")
+        logging.error("Database does not exist.")
+        exit(1)
+
+    try:
+        with sqlite3.connect(db_path) as conn:
+            c = conn.cursor()
+
+            s = c.execute(
+                "SELECT value FROM settings WHERE key = 'settings'"
+            ).fetchone()[0]
+
+            data = json.loads(s)
+            blocks = data["blocks"]
+            blocker_summary(blocks)  # Display blocks summary in terminal
+            blocks = disable_blocker(blocks)
+            data["blocks"] = blocks  # Replace the blocks if modified
+
+            c.execute(
+                """UPDATE settings set value = ? WHERE "key" = 'settings'""",
+                (json.dumps(data),),
+            )
+        conn.commit()
+    except sqlite3.Error:
+        logging.error("Failed to connect with Cold Turkey blocker.")
+        raise
+    finally:
+        os.system("taskkill /F /IM 'ColdTurkey.exe'")  # Terminate the Cold Turkey Blocker process
+
+if __name__ == "__main__":
+    main()
